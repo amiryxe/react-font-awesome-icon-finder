30cce7c36487cb071f2735e26db39f13eb12b9ae
Amir <nik.amirsa@gmail.com>
2021-03-30 13:07:54 +0430

    use React lazy for import IconList component

--- src/components/SearchField/index.js
+++ src/components/SearchField/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
 import MainContext from "../../context/mainContext";
 
 import { Input, Typography } from "antd";
@@ -6,24 +6,18 @@ import { SearchOutlined } from "@ant-design/icons";
 const { Title } = Typography;
 
 const SearchField = (props) => {
-  const [text, setText] = useState("");
-
-  const changeHandler = (e) => {
-    setText(e.target.value);
-  };
+  const { searchFieldText, setSearchFieldText } = useContext(MainContext);
 
   return (
     <div>
-      <MainContext.Provider value={{ searchField: text }}>
-        <Title level={3}>Search</Title>
-        <Input
-          size="large"
-          placeholder="Enter a text to search"
-          onChange={changeHandler}
-          value={text}
-          prefix={<SearchOutlined />}
-        />
-      </MainContext.Provider>
+      <Title level={3}>Search</Title>
+      <Input
+        size="large"
+        placeholder="Enter a text to search"
+        onChange={(e) => setSearchFieldText(e.target.value)}
+        value={searchFieldText}
+        prefix={<SearchOutlined />}
+      />
     </div>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,18 @@
-import React, { Fragment, useContext } from "react";
+import React, { Fragment, useContext, Suspense } from "react";
 import MainContext from "../context/mainContext";
 
 import MenuBar from "../components/MenuBar";
 import SearchField from "../components/SearchField";
-import IconList from "../components/Icons/IconList";
+// import IconList from "../components/Icons/IconList";
 
 import { Row, Col } from "antd";
 import { Layout } from "antd";
 const { Content } = Layout;
 
+const IconList = React.lazy(() => import("../components/Icons/IconList"));
 const Home = () => {
   const { searchFieldText } = useContext(MainContext);
+
   return (
     <Fragment>
       <Layout className="layout">
@@ -27,7 +29,9 @@ const Home = () => {
 
       <Row style={{ marginTop: "2rem" }}>
         <Col span={16} offset={4}>
-          <IconList />
+          <Suspense fallback={<div>Loading...</div>}>
+            <IconList />
+          </Suspense>
         </Col>
       </Row>
     </Fragment>
