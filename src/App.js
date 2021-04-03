ea47d47d0114952de7615151cf2643bef4e9dcce
Amir <nik.amirsa@gmail.com>
2021-04-03 22:42:57 +0430

    initial routing for project and set a path for home and about page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import "./App.css";
-import { Home } from "./pages";
+import { Home, About } from "./pages";
 import MenuBar from "./components/MenuBar";
 import MainState from "./context/MainState";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <MainState>
-      <div className="App">
-        <MenuBar />
-        <Home />
-      </div>
+      <Router>
+        <div className="App">
+          <MenuBar />
+          <Route path="/" exact>
+            <Home />
+          </Route>
+
+          <Route path="/about" exact>
+            <About />
+          </Route>
+        </div>
+      </Router>
     </MainState>
   );
 }
