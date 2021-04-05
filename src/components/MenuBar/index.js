44850eaab8416d8eee5618b8403729d2c99ccf2e
Amir <nik.amirsa@gmail.com>
2021-04-05 10:06:28 +0430

    create link for nav items

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Menu, Layout } from "antd";
+import { Link } from "react-router-dom";
 const { Header } = Layout;
 
 const MenuBar = () => {
@@ -7,8 +8,12 @@ const MenuBar = () => {
     <Header>
       <div className="logo" />
       <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
-        <Menu.Item key="1">Font awesome finder</Menu.Item>
-        <Menu.Item key="2">about</Menu.Item>
+        <Menu.Item key="1">
+          <Link to="/">Font awesome finder</Link>
+        </Menu.Item>
+        <Menu.Item key="2">
+          <Link to="/about">About</Link>
+        </Menu.Item>
       </Menu>
     </Header>
   );
