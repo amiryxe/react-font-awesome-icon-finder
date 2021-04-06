e41436993528d14b006628bf4d583b21062b7672
Amir <nik.amirsa@gmail.com>
2021-04-06 11:26:42 +0430

    create infinty scroll for load icons

--- src/components/Icons/IconList.js
+++ src/components/Icons/IconList.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import IconItem from "./IconItem";
 import { Row, Col } from "antd";
 import MainContext from "../../context/mainContext";
@@ -7,15 +7,46 @@ import InfiniteScroll from "react-infinite-scroll-component";
 const IconList = ({ filter }) => {
   const { iconList } = useContext(MainContext);
 
-  const filteredItems = iconList.filter((itam) => itam.match(filter));
+  const data = iconList.filter((itam) => itam.match(filter));
+
+  const [count, setCount] = useState({
+    prev: 0,
+    next: 10,
+  });
+
+  const [hasMore, setHasMore] = useState(true);
+  const [current, setCurrent] = useState(data.slice(count.prev, count.next));
+  const getMoreData = () => {
+    if (current.length === data.length) {
+      setHasMore(false);
+      return;
+    }
+    setTimeout(() => {
+      setCurrent(current.concat(data.slice(count.prev + 10, count.next + 10)));
+    }, 2000);
+    setCount((prevState) => ({
+      prev: prevState.prev + 10,
+      next: prevState.next + 10,
+    }));
+  };
 
   return (
     <Row>
-      {filteredItems.map((item, index) => (
-        <Col key={index} span={4} style={{ padding: "1rem" }}>
-          <IconItem icon={item} />
-        </Col>
-      ))}
+      <InfiniteScroll
+        dataLength={current.length}
+        next={getMoreData}
+        hasMore={hasMore}
+        loader={<h4>Loading...</h4>}
+      >
+        <div>
+          {current &&
+            current.map((item, index) => (
+              <Col key={index} span={4} style={{ padding: "1rem" }}>
+                <IconItem icon={item} />
+              </Col>
+            ))}
+        </div>
+      </InfiniteScroll>
     </Row>
   );
 };
